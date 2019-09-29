7715f2eaa8dd07194113f735de5334105381bfd8
Jane Jeon <JaneJeon9719@gmail.com>
2019-09-29 08:37:27 -0400

    use service factory to reduce boilerplate

--- dash/index.js
+++ dash/index.js
@@ -1,67 +1,51 @@
 require('dotenv-defaults').config()
 
-const app = require('restana')({
-  errorHandler: require('../middlewares/error-handler')
-})
+const service = require('../lib/service-factory')()
 const passport = require('../lib/passport')
 const ensureLogin = require('../middlewares/ensure-login')
 const Link = require('../models/link')
 
-app.use(require('helmet')())
-app.use(require('cors')({ origin: true }))
-app.use(require('serve-static')('../public', { index: false }))
-app.use(require('../middlewares/session'))
-app.use(require('body-parser').json())
-app.use(passport.initialize())
-app.use(passport.session())
-app.get('/') // TODO:
-app.get(
-  '/login',
-  (req, res) =>
-    req.isAuthenticated() ? res.redirect('/') : res.render('login') // TODO:
-)
-app.get('/logout', (req, res) => {
-  req.logout()
-  req.session.destroy(err => {
-    if (err) console.error('Failed to destroy the session during logout', err)
-    req.user = null
-    res.redirect('/')
-  })
-})
-app.get('/auth/github', passport.authenticate('github'))
-app.get(
-  '/auth/github/callback',
-  passport.authenticate('github', {
-    failureRedirect: '/login',
-    successReturnToOrRedirect: '/'
-  })
-)
-app.get('/links', ensureLogin, async (req, res) => {
-  // TODO:
-})
-app.post('/links', ensureLogin, async (req, res) => {
-  // TODO:
-  const link = await Link.create(
-    Object.assign({ creator: req.user.id }, req.body)
+service
+  .useMiddlewares([
+    require('helmet')(),
+    require('cors')({ origin: true }),
+    require('serve-static')('../public', { index: false }),
+    require('../middlewares/session'),
+    require('body-parser').json(),
+    passport.initialize(),
+    passport.session()
+  ])
+  .get('/') // TODO:
+  .get(
+    '/login',
+    (req, res) =>
+      req.isAuthenticated() ? res.redirect('/') : res.render('login') // TODO:
   )
-  res.send(link, 201)
-})
-
-if (process.env.NODE_ENV !== 'test')
-  app
-    .start(process.env.PORT)
-    .then(server => {
-      const address = server.address()
-      console.log(`Server listening at ${address.address}:${address.port}`)
+  .get('/logout', (req, res) => {
+    req.logout()
+    req.session.destroy(err => {
+      if (err) console.error('Failed to destroy the session during logout', err)
+      req.user = null
+      res.redirect('/')
     })
-    .catch(err => {
-      console.error(err)
-      process.exit(1)
+  })
+  .get('/auth/github', passport.authenticate('github'))
+  .get(
+    '/auth/github/callback',
+    passport.authenticate('github', {
+      failureRedirect: '/login',
+      successReturnToOrRedirect: '/'
     })
+  )
+  .get('/links', ensureLogin, async (req, res) => {
+    // TODO:
+  })
+  .post('/links', ensureLogin, async (req, res) => {
+    // TODO:
+    const link = await Link.create(
+      Object.assign({ creator: req.user.id }, req.body)
+    )
+    res.send(link, 201)
+  })
 
-process.on('SIGINT', () => {
-  app.close().then(() => process.exit())
-})
-
-// for jest testing
-module.exports = app.getServer()
+module.exports = service.server
--- www/index.js
+++ www/index.js
@@ -1,33 +1,17 @@
 require('dotenv-defaults').config()
 
-const app = require('restana')({
-  errorHandler: require('../middlewares/error-handler')
-})
+const service = require('../lib/service-factory')()
 const Link = require('../models/link')
 
-app.use(require('helmet')())
-app.use(require('cors')({ origin: true }))
-app.get('/', (req, res) => res.redirect(process.env.HOMEPAGE))
-app.get('/:hash', async (req, res) => {
-  const link = await Link.findOne(req.params)
-  res.redirect(link.redirectTo)
-})
+service
+  .useMiddlewares([
+    require('helmet')(),
+    require('cors')({ origin: true }) // is this even necessary?
+  ])
+  .get('/', (req, res) => res.redirect(process.env.HOMEPAGE))
+  .get('/:hash', async (req, res) => {
+    const link = await Link.findOne(req.params)
+    res.redirect(link.redirectTo)
+  })
 
-if (process.env.NODE_ENV !== 'test')
-  app
-    .start(process.env.PORT)
-    .then(server => {
-      const address = server.address()
-      console.log(`Server listening at ${address.address}:${address.port}`)
-    })
-    .catch(err => {
-      console.error(err)
-      process.exit(1)
-    })
-
-process.on('SIGINT', () => {
-  app.close().then(() => process.exit())
-})
-
-// for jest testing
-module.exports = app.getServer()
+module.exports = service.server
