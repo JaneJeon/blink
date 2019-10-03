653f5519f618eec680c25cc1d7bd9e4a5ecae18e
Jane Jeon <JaneJeon9719@gmail.com>
2019-10-03 19:30:05 -0400

    add redirect to the end for performance

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const { Router } = require('express')
 
 module.exports = Router()
-  .use('/', require('./redirect'))
   .use('/api', require('./api'))
   .use('/app', require('./app'))
   .use('/auth', require('./auth'))
+  .use('/', require('./redirect'))
