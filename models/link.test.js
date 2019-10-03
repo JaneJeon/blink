ce6c51f0eb254a709d7bcc2c5ad1a6c1ab2f7c16
Jane Jeon <JaneJeon9719@gmail.com>
2019-10-03 15:30:46 -0400

    jest doesn't do soft comparison

--- models/link.test.js
+++ models/link.test.js
@@ -18,7 +18,7 @@ describe('Link', () => {
     id = doc.id
 
     expect(typeof id).toBe('string')
-    expect(id.length).toBeGreaterThanOrEqual(process.env.HASH_MIN_LENGTH)
+    expect(id.length).toBeGreaterThanOrEqual(process.env.HASH_MIN_LENGTH - 0)
   })
 
   test('prevent duplicate URLs', async () => {
