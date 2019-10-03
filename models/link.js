26cf15e17c7a1efa1802d5329030f3d9329099ee
Jane Jeon <JaneJeon9719@gmail.com>
2019-10-03 08:21:26 -0400

    add basic redirects

--- models/link.js
+++ models/link.js
@@ -6,6 +6,21 @@ const normalizeURL = require('normalize-url')
 const HashIds = require('hashids/cjs')
 const hash = new HashIds(process.env.DOMAIN, process.env.HASH_MIN_LENGTH - 0)
 
+const preservedURLs = [
+  'dashboard',
+  'signup',
+  'authorize',
+  'authenticate',
+  'logout',
+  'settings',
+  'verify',
+  'static',
+  'images',
+  'banned',
+  'report',
+  'account'
+]
+
 const schema = new mongoose.Schema(
   {
     _id: {
@@ -27,10 +42,16 @@ const schema = new mongoose.Schema(
       trim: true,
       maxlength: process.env.URL_MAX_LENGTH,
       immutable: true,
-      validate: {
-        validator: url => new URL(url).host !== process.env.DOMAIN,
-        msg: `Cannot shorten ${process.env.DOMAIN} URLs`
-      },
+      validate: [
+        {
+          validator: url => new URL(url).host !== process.env.DOMAIN,
+          msg: `Cannot shorten ${process.env.DOMAIN} URLs`
+        },
+        {
+          validator: url => !preservedURLs.includes(url),
+          msg: 'This URL is preserved'
+        }
+      ],
       set: url => normalizeURL(url, { forceHttps: true })
     },
     creator: {
