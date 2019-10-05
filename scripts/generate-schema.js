731817c71556f7c5d87e8dbee22099b5fa8ab9dc
Jane Jeon <JaneJeon9719@gmail.com>
2019-10-04 20:34:44 -0400

    generate schema using lint-staged

--- scripts/generate-schema.js
+++ scripts/generate-schema.js
@@ -1,25 +1,21 @@
 require('../config')
 
 const fs = require('fs')
+const path = require('path')
 const { execSync } = require('child_process')
 const omit = require('lodash/omit')
 
-const schema = {}
-fs.readdirSync('models')
-  .filter(
-    f =>
-      f.endsWith('.js') && !f.endsWith('.test.js') && !f.startsWith('sequence')
-  )
-  .map(f => f.slice(0, -3))
-  .forEach(model => {
-    schema[model] = omit(require(`../models/${model}`).jsonSchema(), [
-      'properties.createdAt',
-      'properties.updatedAt',
-      'properties.__v'
-    ])
-  })
+let model = process.argv[2]
+if (!model || model.includes('.test')) process.exit()
+model = path.basename(model).split('.')[0]
+
+const schema = require('../config/schema.json') || {}
+const modelSchema = omit(require(`../models/${model}`).jsonSchema(), [
+  'properties.createdAt',
+  'properties.updatedAt',
+  'properties.__v'
+])
+schema[model] = modelSchema
 
 fs.writeFileSync('config/schema.json', JSON.stringify(schema))
 execSync('git add config/schema.json')
-
-process.exit(0) // needed since mongoose won't shut down on its own
