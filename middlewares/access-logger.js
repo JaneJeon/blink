fa0b73932145f379d04458638d996647c80a3d51
Jane Jeon <JaneJeon9719@gmail.com>
2019-10-12 23:02:23 -0400

    bloat

--- middlewares/access-logger.js
+++ middlewares/access-logger.js
@@ -1,10 +1,14 @@
-const morgan = require('morgan')
 const log = require('../lib/logger')
 
-module.exports =
-  process.env.NODE_ENV === 'production'
-    ? (req, res, next) => next()
-    : morgan(process.env.NODE_ENV === 'development' ? 'dev' : 'tiny', {
-        skip: (req, res) => res.statusCode >= 400,
-        stream: { write: msg => log.info(msg.trimRight()) }
-      })
+let accessLogger
+try {
+  const morgan = require('morgan')
+  morgan(process.env.NODE_ENV === 'development' ? 'dev' : 'tiny', {
+    skip: (req, res) => res.statusCode >= 400,
+    stream: { write: msg => log.info(msg.trimRight()) }
+  })
+} catch (err) {
+  accessLogger = (req, res, next) => next()
+}
+
+module.exports = accessLogger
