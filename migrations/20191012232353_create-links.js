f976ac738d7922f85fad473d051a6c8b2831a734
Jane Jeon <JaneJeon9719@gmail.com>
2019-10-19 20:29:24 -0400

    instead of lowercasing, camelCase custom hash

--- migrations/20191012232353_create-links.js
+++ migrations/20191012232353_create-links.js
@@ -4,7 +4,10 @@ exports.up = knex =>
   knex.schema.createTable(tableName, table => {
     table.increments()
     table.text('hash').unique()
-    table.text('original_URL').notNullable()
+    table
+      .text('original_URL')
+      .notNullable()
+      .unique()
     table
       .text('creator_id')
       .references('users.id')
