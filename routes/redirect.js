066e66807c99dd49fd9e0e5e58f11ab3aaef9e21
Jane Jeon <JaneJeon9719@gmail.com>
2020-01-18 17:55:31 -0500

    limit redirect checks to only alphanumerics

--- routes/redirect.js
+++ routes/redirect.js
@@ -3,7 +3,7 @@ const Link = require('../models/link')
 
 module.exports = Router()
   .get('/', (req, res) => res.redirect(301, process.env.HOMEPAGE))
-  .get('/:hash', async (req, res) => {
+  .get('/:hash([a-zA-Z0-9]+)', async (req, res) => {
     const link = await Link.query().findByHashId(req.params.hash)
 
     res.redirect(301, link.originalURL)
