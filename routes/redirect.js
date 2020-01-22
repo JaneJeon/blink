12cac7c2c20914fbd6bb75579d37901468839cd2
Jane Jeon <JaneJeon9719@gmail.com>
2020-01-22 03:47:22 -0500

    limit link matching to only possible hashes

--- routes/redirect.js
+++ routes/redirect.js
@@ -1,9 +1,12 @@
 const { Router } = require('express')
 const Link = require('../models/link')
+const schema = require('../config/schema')
+const hashSchema = schema.Link.properties.hash
+const possibleHash = `/:hash([a-zA-Z0-9]{${hashSchema.minLength},${hashSchema.maxLength}})`
 
 module.exports = Router()
   .get('/', (req, res) => res.redirect(301, process.env.HOMEPAGE))
-  .get('/:hash', async (req, res) => {
+  .get(possibleHash, async (req, res) => {
     const link = await Link.query().findByHashId(req.params.hash)
 
     res.redirect(301, link.originalURL)
