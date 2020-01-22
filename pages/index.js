7efb1feae7af4fb09b31d1655bfa36ccd9246bac
Jane Jeon <JaneJeon9719@gmail.com>
2020-01-22 04:18:19 -0500

    force exit on github actions

--- pages/index.js
+++ pages/index.js
@@ -7,14 +7,6 @@ const Home = () => (
     <Head>
       <title>Home</title>
       <link rel="icon" href="/static/favicon.ico" importance="low" />
-      <link
-        rel="stylesheet"
-        href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-      />
-      {/* <link
-        rel="stylesheet"
-        href="https://stackpath.bootstrapcdn.com/bootswatch/4.3.1/darkly/bootstrap.min.css"
-      /> */}
     </Head>
     <URLForm />
   </div>
