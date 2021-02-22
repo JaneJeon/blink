ea4e237092449d855df4ab724888b20c6887fc62
Jane Jeon <me@janejeon.dev>
2021-02-22 13:46:59 -0500

    fix seeding

--- seeds/20210125230719_clean.js
+++ seeds/20210125230719_clean.js
@@ -1,5 +1,9 @@
 const knexCleaner = require('knex-cleaner')
 
 exports.seed = async knex => {
-  return knexCleaner.clean(knex, { mode: 'truncate', restartIdentity: 'true' })
+  return knexCleaner.clean(knex, {
+    mode: 'truncate',
+    restartIdentity: 'true',
+    ignoreTables: ['knex_migrations', 'knex_migrations_lock']
+  })
 }
