19fb0696d4ad0b2a0a1dabd2f0428e7edd8a3b61
Jane Jeon <me@janejeon.dev>
2021-04-11 14:14:33 -0400

    set cache header for base url as well

--- routes/redirect.js
+++ routes/redirect.js
@@ -5,7 +5,10 @@ const ms = require('ms')
 const cacheAge = ms(process.env.CACHE_MAX_AGE) / 1000 // in seconds
 
 module.exports = Router()
-  .get('/', (req, res) => res.redirect(301, process.env.HOMEPAGE))
+  .get('/', (req, res) => {
+    res.set('Cache-Control', `public, max-age=${cacheAge}`)
+    res.redirect(301, process.env.HOMEPAGE)
+  })
   .get(`/:hash(${schema.Link.properties.hash.pattern})`, async (req, res) => {
     const link = await Link.query()
       .findByHashId(req.params.hash)
