9a0f4997af3be463cd1df2dfda001d08ffaa4d20
Jane Jeon <me@janejeon.dev>
2021-04-11 21:23:53 -0400

    automatically turn on ssl for production

--- knexfile.js
+++ knexfile.js
@@ -1,6 +1,8 @@
 // istanbul ignore file
 require('./config')
 
+const { parse } = require('pg-connection-string')
+
 const log = require('./lib/logger')
 const { knexSnakeCaseMappers } = require('objection')
 
@@ -8,11 +10,9 @@ const env = process.env.NODE_ENV || 'development'
 
 module.exports = {
   client: 'pg',
-  connection: process.env.DATABASE_URL || {
-    // no host or pw, cuz why tf would you use dbs in containers for prod?!?
-    database: process.env.POSTGRES_DB,
-    user: process.env.POSTGRES_USER
-  },
+  connection: Object.assign(parse(process.env.DATABASE_URL), {
+    ssl: env === 'production'
+  }),
   log: {
     warn: msg => log.warn(msg),
     error: msg => log.error(msg),
