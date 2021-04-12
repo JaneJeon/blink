7650bdf4f803a189ac29fbfe16cd7ccf8783397b
Jane Jeon <me@janejeon.dev>
2021-04-11 21:41:53 -0400

    remove rate limit for /app because react

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,6 @@ const rateLimit = require('../middlewares/rate-limiter')
 
 module.exports = Router()
   .use('/api', auth, rateLimit, require('./api'))
-  .use('/app', rateLimit, require('./app'))
+  .use('/app', require('./app'))
   .use('/auth', rateLimit, require('./auth'))
   .use('/', require('./redirect'))
