1a044d09f7dc3107e5c41e1de8d6a75060dda3ca
Jane Jeon <me@janejeon.dev>
2021-08-10 01:21:15 -0400

    WIP convert to react-final-form to maintain parity w/ react-admin and for more composability

--- src/components/shortener.js
+++ src/components/shortener.js
@@ -1,7 +1,6 @@
 import { Fragment, useState } from 'react'
-import { useNotify } from 'react-admin'
+import { useNotify, usePermissions } from 'react-admin'
 import { CopyToClipboard } from 'react-copy-to-clipboard'
-import pick from 'lodash/pick'
 import Dialog from '@material-ui/core/Dialog'
 import DialogContent from '@material-ui/core/DialogContent'
 import DialogTitle from '@material-ui/core/DialogTitle'
@@ -11,50 +10,143 @@ import IconButton from '@material-ui/core/IconButton'
 import AddIcon from '@material-ui/icons/Add'
 import SearchIcon from '@material-ui/icons/Search'
 import FileCopyIcon from '@material-ui/icons/FileCopy'
+import { subject } from '@casl/ability'
+import validator from '../providers/validator'
+import { FORM_ERROR } from 'final-form'
+import { Form, Field } from 'react-final-form'
+// TODO: test all this shit locally
 
 export default function LinkShortener() {
   const [isOpen, setIsOpen] = useState(false)
-  const [link, setLink] = useState({
-    hash: '',
-    originalUrl: '',
-    shortenedUrl: '',
-    brandedUrl: ''
-  })
-  const [isLoading, setIsLoading] = useState(false)
-  const [error, setError] = useState('')
   const notify = useNotify()
+  const { loading: permissionsLoading, permissions } = usePermissions()
 
-  async function shortenLink(e) {
-    e.preventDefault()
-
-    if (isLoading) return // wait until request is done
-    setIsLoading(true)
-
-    const linkBody = link.hash
-      ? pick({ ...link }, ['originalUrl', 'hash'])
-      : pick({ ...link }, ['originalUrl'])
-
+  async function shortenLink(values, form) {
     try {
       const result = await fetch('/api/links', {
         method: 'POST',
-        body: JSON.stringify(linkBody),
+        body: JSON.stringify(values),
         headers: {
           'Content-Type': 'application/json'
         }
       })
       if (!result.ok) throw new Error(await result.text())
-      const { shortenedUrl, brandedUrl } = await result.json()
+      const link = await result.json()
 
-      setLink({ ...link, shortenedUrl, brandedUrl })
-      setError('')
+      form.initialize(link)
       notify('Shortened link!')
     } catch (err) {
-      setError(err.message)
-      notify('Failed to shorten link')
-    } finally {
-      setIsLoading(false)
+      notify(err.message, 'error')
+      return { [FORM_ERROR]: err.message }
     }
   }
+
+  const ShortenerForm = () => (
+    <Form
+      validate={validator('Link')}
+      onSubmit={shortenLink}
+      render={({ handleSubmit, submitting, values, pristine, invalid }) => {
+        const resource = subject('Link', values)
+
+        return (
+          <form onSubmit={handleSubmit}>
+            <Field name="originalUrl">
+              {({ input, meta }) => (
+                <>
+                  <TextField
+                    name={input.name}
+                    value={input.value}
+                    onChange={input.onChange}
+                    autoFocus
+                    variant="filled"
+                    label="Paste link to shorten"
+                    placeholder="example.com"
+                    error={meta.invalid}
+                    helperText={meta.error}
+                    disabled={
+                      submitting || permissions.cannot('create', resource) // check if you're allowed to shorten link
+                    }
+                    data-testid="originalUrl-field"
+                    InputProps={{
+                      endAdornment: (
+                        <InputAdornment position="end">
+                          <IconButton
+                            aria-label="shorten"
+                            data-testid="shorten-button"
+                            color="secondary"
+                            disabled={submitting || pristine || invalid}
+                            type="submit"
+                          >
+                            <SearchIcon />
+                          </IconButton>
+                        </InputAdornment>
+                      )
+                    }}
+                  />
+                </>
+              )}
+            </Field>
+            <Field name="hash">
+              {({ input, meta }) => (
+                <>
+                  <TextField
+                    name={input.name}
+                    value={input.value}
+                    onChange={input.onChange}
+                    variant="filled"
+                    label="Custom URL"
+                    placeholder={
+                      values.shortenedUrl
+                        ? values.shortenedUrl.split('/').pop()
+                        : 'awesome-link'
+                    }
+                    data-testid="hash-field"
+                    error={meta.invalid}
+                    helperText={meta.error}
+                    disabled={
+                      submitting ||
+                      (!values.hash &&
+                        permissions.cannot('create', resource, 'hash')) || // if the hash is not set, deny if you can't create hash
+                      (values.hash &&
+                        permissions.cannot('update', resource, 'hash')) // if the hash is set, deny if you can't update hash
+                    }
+                    style={{ marginTop: '2rem', paddingBottom: '1rem' }}
+                    InputProps={{
+                      startAdornment: (
+                        <InputAdornment
+                          position="start"
+                          style={{ marginRight: 0, marginBottom: '-3px' }}
+                        >
+                          {process.env.REACT_APP_BASE_URL}/
+                        </InputAdornment>
+                      ),
+                      endAdornment: (
+                        <InputAdornment position="end">
+                          <CopyToClipboard
+                            text={values.brandedUrl || values.shortenedUrl}
+                            data-testid="copy-button"
+                          >
+                            <IconButton
+                              aria-label="copy link"
+                              color="secondary"
+                              disabled={submitting || !values.shortenedUrl} // disable copy when there's nothing to copy
+                              onClick={() => notify('Link copied!')}
+                            >
+                              <FileCopyIcon />
+                            </IconButton>
+                          </CopyToClipboard>
+                        </InputAdornment>
+                      )
+                    }}
+                  />
+                </>
+              )}
+            </Field>
+          </form>
+        )
+      }}
+    />
+  )
 
   return (
     <Fragment>
@@ -66,7 +158,6 @@ export default function LinkShortener() {
       >
         <AddIcon />
       </IconButton>
-
       <Dialog
         open={isOpen}
         onClose={() => setIsOpen(false)}
@@ -75,78 +166,8 @@ export default function LinkShortener() {
         fullWidth={true}
       >
         <DialogTitle id="form-dialog-title">Shorten Link</DialogTitle>
-
         <DialogContent>
-          <form onSubmit={shortenLink}>
-            <TextField
-              autoFocus
-              variant="filled"
-              label="Paste link to shorten"
-              placeholder="example.com"
-              onChange={e => setLink({ ...link, originalUrl: e.target.value })}
-              error={!!error}
-              helperText={error}
-              disabled={isLoading}
-              data-testid="originalUrl-field"
-              InputProps={{
-                endAdornment: (
-                  <InputAdornment position="end">
-                    <IconButton
-                      aria-label="shorten"
-                      data-testid="shorten-button"
-                      color="secondary"
-                      disabled={isLoading} // TODO: json schema check originalUrl
-                      type="submit"
-                    >
-                      <SearchIcon />
-                    </IconButton>
-                  </InputAdornment>
-                )
-              }}
-            />
-            <TextField
-              variant="filled"
-              label="Custom URL"
-              placeholder={
-                link.shortenedUrl
-                  ? link.shortenedUrl.split('/').pop()
-                  : 'awesome-link'
-              }
-              value={link.brandedUrl ? link.brandedUrl.split('/').pop() : ''}
-              data-testid="hash-field"
-              onChange={e => setLink({ ...link, hash: e.target.value })}
-              error={!!error}
-              disabled={isLoading || !!link.brandedUrl} // TODO: disable when you can't change
-              style={{ marginTop: '2rem', paddingBottom: '1rem' }}
-              InputProps={{
-                startAdornment: (
-                  <InputAdornment
-                    position="start"
-                    style={{ marginRight: 0, marginBottom: '-3px' }}
-                  >
-                    {process.env.REACT_APP_BASE_URL}/
-                  </InputAdornment>
-                ),
-                endAdornment: (
-                  <InputAdornment position="end">
-                    <CopyToClipboard
-                      text={link.brandedUrl || link.shortenedUrl}
-                      data-testid="copy-button"
-                    >
-                      <IconButton
-                        aria-label="copy link"
-                        color="secondary"
-                        disabled={isLoading || !link.shortenedUrl}
-                        onClick={() => notify('Link copied!')}
-                      >
-                        <FileCopyIcon />
-                      </IconButton>
-                    </CopyToClipboard>
-                  </InputAdornment>
-                )
-              }}
-            />
-          </form>
+          {permissionsLoading ? <div>Loading...</div> : <ShortenerForm />}
         </DialogContent>
       </Dialog>
     </Fragment>
