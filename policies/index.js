656f4f192ec8ab572414011b4cd77a6d732f13f4
Jane Jeon <me@janejeon.dev>
2021-08-11 00:43:14 -0400

    make policy act like browser only when testing CRA

--- policies/index.js
+++ policies/index.js
@@ -5,16 +5,18 @@ const { AbilityBuilder, Ability } = require('@casl/ability')
 // This can be updated with import syntax once #128 is under way.
 const policyMap = { Link: require('./link'), User: require('./user') }
 
+// https://github.com/flexdinesh/browser-or-node/blob/master/src/index.js
+const isRunningInNode =
+  typeof process !== 'undefined' &&
+  process.versions != null &&
+  process.versions.node != null
+const isReactTest =
+  process.env.REACT_TEST === 'true' || process.env.REACT_TEST === '1'
+
 module.exports = (user, resource, action, body, opts, relation) => {
   const { rules, can: allow, cannot: forbid } = new AbilityBuilder(Ability)
 
-  // https://github.com/flexdinesh/browser-or-node/blob/master/src/index.js
-  const isRunningInNode =
-    typeof process !== 'undefined' &&
-    process.versions != null &&
-    process.versions.node != null
-
-  if (isRunningInNode) {
+  if (isRunningInNode && !isReactTest) {
     // For use in objection-authorize
     const policies = policyMap[resource.constructor.name] // TODO: handle relations
     policies[action](allow, forbid, user, body)
