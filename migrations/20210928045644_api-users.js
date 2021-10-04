3706a91509c7e49bcf4120b2631797a3751f3c6f
Jane Jeon <me@janejeon.dev>
2021-10-03 23:14:46 -0400

    clean

--- migrations/20210928045644_api-users.js
+++
@@ -1,9 +0,0 @@
-const { tableName } = require('../models/user')
-
-exports.up = async knex => {
-  await knex.schema.table(tableName, table => {
-    table.string('')
-  })
-}
-
-exports.down = async knex => {}
