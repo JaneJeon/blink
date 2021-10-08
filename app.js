e5efe51f85fae5973b68be51626487b5caf6e44d
Jane Jeon <me@janejeon.dev>
2021-10-08 13:52:33 -0400

    closes #594

--- app.js
+++ app.js
@@ -5,11 +5,18 @@ require('express-async-errors')
 const express = require('express')
 const logger = require('./lib/logger')
 const passport = require('./middlewares/passport')
+const { scan } = require('@hapi/bourne')
 
 module.exports = express()
   .set('trust proxy', true)
   .use(require('./middlewares/security'))
-  .use(express.json())
+  .use(
+    express.json({
+      verify: (req, res, buf, encoding) => {
+        scan(req.body)
+      }
+    })
+  )
   .use(require('express-query-boolean')())
   .use(require('./middlewares/session'))
   .use(passport.initialize())
