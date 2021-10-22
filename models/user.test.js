55ebfaea21278c5dd072ba93b457be79cc728366
Jane Jeon <me@janejeon.dev>
2021-10-21 21:32:17 -0400

    fix tests

--- models/user.test.js
+++ models/user.test.js
@@ -2,9 +2,12 @@ require('../__utils__/knex-test')
 require('../__utils__/trxify-test')
 
 const User = require('./user')
+const { API_USER_ID } = require('../config/constants')
 
 describe('User model', () => {
   test('the first non-API user gets elevated default role', async () => {
+    await User.query().whereNot({ id: API_USER_ID }).delete()
+
     // By the time we do the tests, the DB is already seeded with the API "user"
     const role1 = await User.defaultRole()
     expect(role1).toBe('superuser')
