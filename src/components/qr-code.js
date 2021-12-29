3d290d40b3ce6803b267079e6c0954a732a20a17
Jane Jeon <me@janejeon.dev>
2021-12-28 19:26:31 -0500

    fix memory leak

--- src/components/qr-code.js
+++ src/components/qr-code.js
@@ -1,14 +1,28 @@
 import QRCode from 'qrcode'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 export default function QRCodeDisplay({ link }) {
   const [dataUrl, setDataUrl] = useState('')
   const [error, setError] = useState('')
 
-  if (link)
-    QRCode.toDataURL(link)
-      .then(setDataUrl)
-      .catch(err => setError(err.message))
+  // We can't just QRCode.toDataURL().then(setState).catch(setState)
+  // because this component might be unmounted by then.
+  useEffect(() => {
+    let isMounted = true
+
+    if (link)
+      QRCode.toDataURL(link)
+        .then(url => {
+          if (isMounted) setDataUrl(url)
+        })
+        .catch(err => {
+          if (isMounted) setError(err.message)
+        })
+
+    return () => {
+      isMounted = false
+    }
+  }, [link])
 
   if (error) return <p style={{ color: 'red' }}>{error}</p>
   if (dataUrl) return <img src={dataUrl} alt="QR Code" />
