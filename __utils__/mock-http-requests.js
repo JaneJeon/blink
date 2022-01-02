7522ffe0aab8e36cf546d7f6a67b0d3f499ea04d
Jane Jeon <me@janejeon.dev>
2022-01-01 22:02:15 -0500

    mock timeout for http requests (#697)
    * mock timeout for http requests
    
    * set timeouts
    
    * whoops
    
    * ?
    
    * try
    
    * enable connections
    
    * wtf is going on here
    
    * close more shit after close
    
    * well, I tried

---
+++ __utils__/mock-http-requests.js
@@ -0,0 +1,31 @@
+const nock = require('nock')
+
+nock.disableNetConnect()
+nock.enableNetConnect('127.0.0.1')
+
+// Mock HTTP timeouts
+;['timeout.com', 'www.timeout.com'].forEach(url => {
+  nock(url).get('/').delay(100000).reply(200, '<html></html>').persist()
+})
+
+// For a couple of "stock" websites, prevent actually hitting them
+;['nodejs.org', 'google.com', 'example.com', 'js.org'].forEach(url => {
+  nock(url)
+    .get('/')
+    .reply(
+      200,
+      `<!doctype html>
+      <html lang="en">
+        <head>
+          <meta charset="utf-8">
+          <title>Some Title</title>
+          <meta name="description" content="Some Description">
+          <meta name="author" content="Some Author">
+        </head>
+        <body>
+          <p>Hello!</p>
+        </body>
+      </html>`
+    )
+    .persist()
+})
