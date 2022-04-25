f5ac9174cb928578bb89128b51fd8f8ac1d0b091
Jane Jeon <git@janejeon.com>
2022-04-25 13:51:01 -0400

    update major backend dependencies

--- middlewares/rate-limiter.js
+++ middlewares/rate-limiter.js
@@ -5,7 +5,7 @@ const client = require('../lib/redis')
 const { JWT_AUTH_PROPERTY } = require('../config/constants')
 
 exports.short = rateLimit({
-  headers: true,
+  legacyHeaders: true,
   windowMs: ms(process.env.RATE_LIMIT_SHORT_WINDOW),
   max: process.env.RATE_LIMIT_SHORT_MAX - 0,
   keyGenerator: req =>
@@ -14,10 +14,14 @@ exports.short = rateLimit({
 })
 
 exports.long = rateLimit({
-  headers: true,
+  legacyHeaders: true,
   windowMs: ms(process.env.RATE_LIMIT_LONG_WINDOW),
   max: process.env.RATE_LIMIT_LONG_MAX - 0,
   keyGenerator: req =>
     (req[JWT_AUTH_PROPERTY] || {}).sub || (req.user || {}).id || req.ip,
-  store: new RedisStore({ client, prefix: 'rate-limit-long' })
+  store: new RedisStore({
+    prefix: 'rate-limit-long',
+    // See: https://github.com/wyattjoh/rate-limit-redis#examples
+    sendCommand: (...args) => client.call(...args)
+  })
 })
