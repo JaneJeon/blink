642cfe2bd0d252a0aea841bb06ec22c2418ff700
Jane Jeon <git@janejeon.com>
2023-03-16 13:58:03 +0700

    fix CSP issue

--- middlewares/security.js
+++ middlewares/security.js
@@ -4,7 +4,11 @@ module.exports = helmet({
   contentSecurityPolicy: {
     directives: {
       ...helmet.contentSecurityPolicy.getDefaultDirectives(),
-      'img-src': ["'self'", '*.unsplash.com']
+      // This CSP policy controls which images can be loaded on the web page.
+      // The 'self' is self-evident, the unsplash.com is for the background image of the login page,
+      // and the data: is for displaying QR code images, which are loaded as `data:` URLs:
+      // https://stackoverflow.com/a/18449556
+      'img-src': ["'self'", 'data:', '*.unsplash.com']
     }
   }
 })
